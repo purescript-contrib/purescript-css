03a01c07e719e3e753fc20dbdac4f68592c56f02
Brian McKenna <brian@brianmckenna.org>
2015-04-08 21:29:45 -0600

    Make tests into actual unit tests

--- test/Main.purs
+++ test/Main.purs
@@ -1,24 +1,33 @@
 module Main where
 
+import Control.Monad
 import Control.Monad.Eff
+import Control.Monad.Eff.Exception
 import Css.Display
-import Css.String
 import Css.Font
 import Css.Render
-import Debug.Trace
+import Css.String
 import Data.These
+import Data.Maybe
+import Debug.Trace
 
-example :: Rendered
-example = render do
+example1 :: Rendered
+example1 = render do
   color $ fromString "red"
   display block
 
-printResult :: Rendered -> Eff (trace :: Trace) Unit
-printResult (This (Inline a)) = trace a
-printResult (That (Sheet a))  = trace a
-printResult (Both (Inline a) (Sheet b))  = do
-  trace a
-  trace b
+example2 :: Rendered
+example2 = render do
+  display inlineBlock
 
-main :: Eff (trace :: Trace) Unit
-main = printResult example
+inlineResult :: Rendered -> Maybe String
+inlineResult (This (Inline a)) = Just a
+inlineResult _ = Nothing
+
+assertEqual :: forall a. (Eq a, Show a) => a -> a -> Eff (err :: Exception) Unit
+assertEqual x y = unless (x == y) <<< throwException <<< error $ "Assertion failed: " <> show x <> " /= " <> show y
+
+main :: Eff (err :: Exception) Unit
+main = do
+  inlineResult example1 `assertEqual` Just "color: red; display: block"
+  inlineResult example2 `assertEqual` Just "display: inline-block"
