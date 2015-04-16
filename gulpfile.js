ae8aa59ae1260c0e1340bac3e44470137b02f90b
Brian McKenna <brian@brianmckenna.org>
2015-04-15 20:09:47 -0600

    Use gulp-gh-pages for the site example

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp')
   , browserify  = require('gulp-browserify')
-  , purescript  = require('gulp-purescript');
+  , purescript  = require('gulp-purescript')
+  , ghPages = require('gulp-gh-pages');
 
 require("mandragora-bucket")(gulp);
 
 gulp.task('site', function() {
-    return gulp.src(['site/Main.purs', 'src/**/*.purs', 'bower_components/purescript-*/src/**/*.purs']).pipe(purescript.psc({
-        main: 'Site',
-        modules: ['Site']
-    })).pipe(browserify({})).pipe(gulp.dest('site'));
+  return gulp.src(['site/Main.purs', 'src/**/*.purs', 'bower_components/purescript-*/src/**/*.purs']).pipe(purescript.psc({
+    main: 'Site',
+    modules: ['Site']
+  })).pipe(browserify({})).pipe(gulp.dest('site'));
+});
+
+gulp.task('deploy', ['site'], function() {
+  return gulp.src('site/**/*')
+    .pipe(ghPages());
 });
