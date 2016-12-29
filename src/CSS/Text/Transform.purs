039fdde54f1d95015227d3a1af763a8d182cd936
Alex Mingoia <talk@alexmingoia.com>
2016-12-29 13:27:22 -0800

    Use gShow.

--- src/CSS/Text/Transform.purs
+++ src/CSS/Text/Transform.purs
@@ -7,14 +7,14 @@ module CSS.Text.Transform
   ) where
 
 import CSS.Common (class Inherit, class Initial, class None)
-import CSS.Property (class Val, value)
+import CSS.Property (class Val)
 import CSS.String (fromString)
 import CSS.Stylesheet (CSS, key)
 import Data.Eq (class Eq)
 import Data.Function (($))
-import Data.Generic (class Generic)
+import Data.Generic (class Generic, gShow)
 import Data.Ord (class Ord)
-import Data.Show (class Show, show)
+import Data.Show (class Show)
 
 data TextTransform
   = Uppercase
@@ -29,15 +29,15 @@ derive instance ordTextTransform :: Ord TextTransform
 derive instance genericTextTransform :: Generic TextTransform
 
 instance valTextTransform :: Val TextTransform where
-  value t = value (show t)
+  value (Uppercase)  = fromString "uppercase"
+  value (Lowercase)  = fromString "lowercase"
+  value (Capitalize) = fromString "capitalize"
+  value (None)       = fromString "none"
+  value (Initial)    = fromString "initial"
+  value (Inherit)    = fromString "inherit"
 
-instance showTextTransform :: Show TextTransform where
-  show (Uppercase)  = "uppercase"
-  show (Lowercase)  = "lowercase"
-  show (Capitalize) = "capitalize"
-  show (None)       = "none"
-  show (Initial)    = "initial"
-  show (Inherit)    = "inherit"
+instance showTextTransform :: Show (TextTransform) where
+  show = gShow
 
 instance noneTextTransform :: None TextTransform where
   none = None
