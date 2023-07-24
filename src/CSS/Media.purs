2076130450c1d217def5f8205d736d5694344a70
qwbarch <42880469+qwbarch@users.noreply.github.com>
2023-07-23 23:33:24 -0400

    Add minWidth media query

--- src/CSS/Media.purs
+++ src/CSS/Media.purs
@@ -12,5 +12,8 @@ import CSS.Stylesheet (Feature(..), MediaType(..))
 screen :: MediaType
 screen = MediaType $ fromString "screen"
 
+minWidth :: Size LengthUnit -> Feature
+minWidth = Feature "min-width" <<< Just <<< value
+
 maxWidth :: Size LengthUnit -> Feature
 maxWidth = Feature "max-width" <<< Just <<< value
