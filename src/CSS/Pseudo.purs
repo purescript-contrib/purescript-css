20ea0f9d7f62b38283874c91e19b6022cb031e01
qwbarch <42880469+qwbarch@users.noreply.github.com>
2023-07-24 20:59:27 -0400

    Add before/after pseudo selectors

--- src/CSS/Pseudo.purs
+++ src/CSS/Pseudo.purs
@@ -5,3 +5,9 @@ import CSS.String (fromString)
 
 hover :: Refinement
 hover = fromString ":hover"
+
+before :: Refinement
+before = fromString "::before"
+
+after :: Refinement
+after = fromString "::after"
